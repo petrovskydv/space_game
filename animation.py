9e28da682a24a37a97ce51c3151ebb65538c05f7
Денис Петровский <petrovsky.dv@gmail.com>
2021-06-05 19:19:01 +0300

    replace functions

--- animation.py
+++ animation.py
@@ -2,13 +2,9 @@ import asyncio
 import curses
 import random
 
+from curses_tools import draw_frame, get_frame_size, read_controls
+from obstacles import Obstacle, show_obstacles
 from physics import update_speed
-
-SPACE_KEY_CODE = 32
-LEFT_KEY_CODE = 260
-RIGHT_KEY_CODE = 261
-UP_KEY_CODE = 259
-DOWN_KEY_CODE = 258
 
 ROW = 1
 COLUMN = 40
@@ -59,38 +55,6 @@ async def fire(canvas, start_row, start_column, rows_speed=-0.1, columns_speed=0
         column += columns_speed
 
 
-def draw_frame(canvas, start_row, start_column, text, negative=False):
-    """Draw multiline text fragment on canvas, erase text instead of drawing if negative=True is specified."""
-
-    rows_number, columns_number = canvas.getmaxyx()
-
-    for row, line in enumerate(text.splitlines(), round(start_row)):
-        if row < 0:
-            continue
-
-        if row >= rows_number:
-            break
-
-        for column, symbol in enumerate(line, round(start_column)):
-            if column < 0:
-                continue
-
-            if column >= columns_number:
-                break
-
-            if symbol == ' ':
-                continue
-
-            # Check that current position it is not in a lower right corner of the window
-            # Curses will raise exception in that case. Don`t ask why…
-            # https://docs.python.org/3/library/curses.html#curses.window.addch
-            if row == rows_number - 1 and column == columns_number - 1:
-                continue
-
-            symbol = symbol if not negative else ' '
-            canvas.addch(row, column, symbol)
-
-
 async def animate_spaceship(canvas, frames_cycle, fire_coroutines, timeout=1):
     multiplier = 0.7
     rows_number, columns_number = canvas.getmaxyx()
@@ -124,48 +88,8 @@ async def animate_spaceship(canvas, frames_cycle, fire_coroutines, timeout=1):
         await sleep(int(timeout * multiplier))
 
 
-def read_controls(canvas):
-    """Read keys pressed and returns tuple with controls state."""
-
-    rows_direction = columns_direction = 0
-    space_pressed = False
-
-    while True:
-        pressed_key_code = canvas.getch()
-
-        if pressed_key_code == -1:
-            # https://docs.python.org/3/library/curses.html#curses.window.getch
-            break
-
-        if pressed_key_code == UP_KEY_CODE:
-            rows_direction = -1
-
-        if pressed_key_code == DOWN_KEY_CODE:
-            rows_direction = 1
-
-        if pressed_key_code == RIGHT_KEY_CODE:
-            columns_direction = 1
-
-        if pressed_key_code == LEFT_KEY_CODE:
-            columns_direction = -1
-
-        if pressed_key_code == SPACE_KEY_CODE:
-            space_pressed = True
-
-    return rows_direction, columns_direction, space_pressed
-
-
-def get_frame_size(text):
-    """Calculate size of multiline text fragment, return pair — number of rows and colums."""
-
-    lines = text.splitlines()
-    rows = len(lines)
-    columns = max([len(line) for line in lines])
-    return rows, columns
-
-
-async def fly_garbage(canvas, column, garbage_frame, speed=0.5, timeout=1):
-    """Animate garbage, flying from top to bottom. Сolumn position will stay same, as specified on start."""
+async def fly_garbage(canvas, column, garbage_frame, speed=0.5, timeout=1, obstacle=None):
+    """Animate garbage, flying from top to bottom. Column position will stay same, as specified on start."""
     rows_number, columns_number = canvas.getmaxyx()
 
     column = max(column, 0)
